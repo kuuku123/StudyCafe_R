01587adaf1bfc6cb7a548ce5553fb2dcab86a3f2
kuuku123 <dohyun0047@gmail.com>
2022-07-04 12:54:02 +0900

    add sign-up page + Account entity, controller ,test code for (sign-up access)

---
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.StudyCafe_R.StudyCafe_R.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/","/login","/sign-up","/check-email","/check-email-token",
+                        "email-login","/check-email-login","/login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
---
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/domain/Account.java
@@ -0,0 +1,55 @@
+package com.StudyCafe_R.StudyCafe_R.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified; // check if current account is verified with email account
+
+    private String emailCheckToken; // token used for email verification
+
+    private LocalDateTime joinedAt; // specific time when email verification succeed and consider this user is signed in
+
+    //client's extra info
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location; // varchar(255) above all info
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
