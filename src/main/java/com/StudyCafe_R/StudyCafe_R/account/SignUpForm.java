ed7bd81c5e6b3b2d02e807a5bff67ff898df123a
kuuku123 <dohyun0047@gmail.com>
2022-07-04 15:22:18 +0900

    signUpForm template + testcode fixed

---
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/account/SignUpForm.java
@@ -0,0 +1,13 @@
+package com.StudyCafe_R.StudyCafe_R.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
--- src/main/java/com/StudyCafe_R/StudyCafe_R/config/SecurityConfig.java
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.StudyCafe_R.StudyCafe_R.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +22,10 @@ public class SecurityConfig {
                 .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
                 .anyRequest().authenticated();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()));
     }
 }
