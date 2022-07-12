5d4496d0fbcdd8bf43ebd216738528ff0071577a
kuuku123 <dohyun0047@gmail.com>
2022-07-12 11:15:25 +0900

    @Valid +signUpForm

---
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/account/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.StudyCafe_R.StudyCafe_R.account.repository;
+
+import com.StudyCafe_R.StudyCafe_R.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account,Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickName(String nickname);
+}
