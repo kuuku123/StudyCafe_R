fd70f3b5a3a83178003e6654eafb42488b0a9f7d
kuuku123 <dohyun0047@gmail.com>
2022-08-31 10:01:42 +0900

    notification domain

---
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/modules/notification/Notification.java
@@ -0,0 +1,36 @@
+package com.StudyCafe_R.StudyCafe_R.modules.notification;
+
+import com.StudyCafe_R.StudyCafe_R.modules.account.domain.Account;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Notification {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+    
+
+
+}
