0b2dd7e653381afd8100e6f5dfd12abf90ccc9eb
kuuku123 <dohyun0047@gmail.com>
2022-09-02 13:43:00 +0900

    using HandlerInterceptor now we can get notification for every redering page if it exisits

--- src/main/java/com/StudyCafe_R/StudyCafe_R/modules/notification/NotificationRepository.java
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.StudyCafe_R.StudyCafe_R.modules.notification;
 
+import com.StudyCafe_R.StudyCafe_R.modules.account.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
+    long countByAccountAndChecked(Account account, boolean b);
 }
