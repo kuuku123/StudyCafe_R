9d70b8d36a6d7c4a4c5bcf4644e4bd56711b9387
kuuku123 <dohyun0047@gmail.com>
2022-09-04 15:54:15 +0900

    add enrollment event listener

--- src/main/java/com/StudyCafe_R/StudyCafe_R/modules/event/EventService.java
+++ src/main/java/com/StudyCafe_R/StudyCafe_R/modules/event/EventService.java
@@ -3,10 +3,14 @@ package com.StudyCafe_R.StudyCafe_R.modules.event;
 import com.StudyCafe_R.StudyCafe_R.modules.account.domain.Account;
 import com.StudyCafe_R.StudyCafe_R.modules.event.domain.Enrollment;
 import com.StudyCafe_R.StudyCafe_R.modules.event.domain.Event;
+import com.StudyCafe_R.StudyCafe_R.modules.event.event.EnrollmentAcceptedEvent;
+import com.StudyCafe_R.StudyCafe_R.modules.event.event.EnrollmentRejectedEvent;
 import com.StudyCafe_R.StudyCafe_R.modules.event.form.EventForm;
 import com.StudyCafe_R.StudyCafe_R.modules.study.domain.Study;
+import com.StudyCafe_R.StudyCafe_R.modules.study.event.StudyUpdateEvent;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,20 +25,28 @@ public class EventService {
     private final EnrollmentRepository enrollmentRepository;
     private final ModelMapper modelMapper;
 
+    private final ApplicationEventPublisher applicationEventPublisher;
+
     public Event createEvent(Event event, Study study, Account account) {
         event.setCreatedBy(account);
         event.setCreatedDateTime(LocalDateTime.now());
         event.setStudy(study);
+        applicationEventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                "'" + event.getTitle() + "' 모임을 만들었습니다."));
         return eventRepository.save(event);
     }
 
     public void updateEvent(Event event, EventForm eventForm) {
         modelMapper.map(eventForm,event);
         event.acceptWaitingList();
+        applicationEventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                "'" + event.getTitle() + "' 모임정보를 수정했으니 확인해주세요."));
     }
 
     public void deleteEvent(Event event) {
         eventRepository.delete(event);
+        applicationEventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                "'" + event.getTitle() + "' 모임을 취소하였습니다."));
     }
 
     public void newEnrollment(Event event, Account account) {
@@ -67,9 +79,11 @@ public class EventService {
 
     public void acceptEnrollment(Event event, Enrollment enrollment) {
         event.accept(enrollment);
+        applicationEventPublisher.publishEvent(new EnrollmentAcceptedEvent(enrollment));
     }
 
     public void rejectEnrollment(Event event, Enrollment enrollment) {
         event.reject(enrollment);
+        applicationEventPublisher.publishEvent(new EnrollmentRejectedEvent(enrollment));
     }
 }
